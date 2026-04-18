35ad3e545f9e3f1530fd9f69aa9e4a87c88b1d5b
shada <shada@SHADAB>
2026-04-18 14:23:43 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer speak = new SpeechSynthesizer();
+
+            Console.Write("Enter text: ");
+            string text = Console.ReadLine();
+
+            speak.Speak(text);
+        }
+    }
+}
